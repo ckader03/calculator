d1e8ec22875305c8258110a77b1a1f1385723e0c
ckader03 <50424162+ckader03@users.noreply.github.com>
2019-05-13 21:47:53 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# calculator
+# shopping-list
--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-function input(button){
-document.calc.display.value += button.value;
-}
+$(document).ready(function(){
 
-function clearDisplay(){
-  document.calc.display.value = "";
-}
+// add an event listener that listens for a click on the id #add
+$('#add').click(function(){
 
-function enter(){
-  document.calc.display.value = eval(document.calc.display.value);
-}
+// grab the user input
+var userInput = $('input[name=shoppinglistitem]').val();
 
+// add user input to the DOM as a list item
+$('.list').append('<li>' + '<input class="checked-item" type="checkbox">' + userInput + '</li>');
+return false;
+})
 
-
-function average(){
-  var sum = 0;
-
-  listOfNums = document.calc.display.value.split(',');
-
-for(var i =0; i< listOfNums.length; i++){
-  sum += parseInt(listOfNums[i]);
-}
+// keep track of the user toggling each item and create an event listener
+$(document).on('click','.checked-item',function(){
 
 
-var average = sum/listOfNums.length;
+  // capture the current list item user is clicking
+$(this).parent().toggleClass('is-it-checked');
+ })
 
-document.calc.display.value = average;
-}
+
+//  add an event listener to the check button
+$('.checked').on('click',function(e){
+
+  // prevent the default browser/form action from happening
+  e.preventDefault();
+
+  // remove the checked items
+  $('.is-it-checked').remove();
+
+ }) 
+})
+// add an event listener to the reset button
+$('.reset-list').on('click',function(e){
+
+  e.preventDefault();
+
+// remove all list item
+  $('li').remove();
 
 
+})
+// })
 
